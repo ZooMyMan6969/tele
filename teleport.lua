caf3e46485bb2d7233cd5bf40eb6b4e58e69d86b
ZooMyMan6969 <doomiesteaks@gmail.com>
2024-08-20 10:57:37 +0800

    Update teleport.lua

--- teleport.lua
+++ teleport.lua
@@ -1,4 +1,5 @@
 local my_utility = require("my_utility/my_utility");
+local my_target_selector = require("my_utility/my_target_selector");
 
 local menu_elements_sorc_base = 
 {
@@ -17,7 +18,6 @@ local menu_elements_sorc_base =
 }
 
 local function menu()
-    
     if menu_elements_sorc_base.tree_tab:push("Teleport") then
         menu_elements_sorc_base.main_boolean:render("Enable Spell", "");
 
@@ -39,47 +39,49 @@ local function menu()
     end
 end
 
-local my_target_selector = require("my_utility/my_target_selector");
-
 local spell_id_tp = 288106;
 
 local spell_radius = 2.5;
 local spell_max_range = 10.0;
 
 local next_time_allowed_cast = 0.0;
-        local current_time = get_time_since_inject();
-        next_time_allowed_cast = current_time + 0.5;
+local was_toggle_on = false;
 
 local function logics(entity_list, target_selector_data, best_target)
- 
+    local current_time = get_time_since_inject();
     local menu_boolean = menu_elements_sorc_base.main_boolean:get();
+
+    -- Check if the toggle was just turned on
+    if menu_boolean and not was_toggle_on then
+        next_time_allowed_cast = current_time;
+        was_toggle_on = true;
+    elseif not menu_boolean then
+        was_toggle_on = false;
+    end
+
+    -- Update next_time_allowed_cast
+    next_time_allowed_cast = math.max(next_time_allowed_cast, current_time + 0.01);
+
     local is_logic_allowed = my_utility.is_spell_allowed(
                 menu_boolean, 
                 next_time_allowed_cast, 
                 spell_id_tp);
                 
-
     if not is_logic_allowed then
         return false;
-    end;
+    end
 
     local player_position = get_player_position()
     local keybind_used = menu_elements_sorc_base.keybind:get_state();
-    -- console.print("is keybind used", keybind_used)
     local teleport_mode = menu_elements_sorc_base.teleport_mode:get();
-    -- console.print("is tp used", teleport_mode)
     if teleport_mode == 1 then
-        if  keybind_used == 0 then   
+        if keybind_used == 0 then   
             return false;
-        end;
-    end;
+        end
+    end
 
     local keybind_ignore_hits = menu_elements_sorc_base.keybind_ignore_hits:get();
-
-    ---@type boolean
-        local keybind_can_skip = keybind_ignore_hits == true and keybind_used > 0;
-    -- console.print("keybind_can_skip " .. tostring(keybind_can_skip))
-    -- console.print("keybind_used " .. keybind_used)
+    local keybind_can_skip = keybind_ignore_hits == true and keybind_used > 0;
     
     local is_percentage_hits_allowed = menu_elements_sorc_base.allow_percentage_hits:get();
     local min_percentage = menu_elements_sorc_base.min_percentage_hits:get();
@@ -96,7 +98,7 @@ local function logics(entity_list, target_selector_data, best_target)
     
     local is_area_valid = my_target_selector.is_valid_area_spell_aio(area_data, min_hits_menu, entity_list, min_percentage);
 
-    if not is_area_valid and not keybind_can_skip  then
+    if not is_area_valid and not keybind_can_skip then
         return false;
     end
 
@@ -112,24 +114,21 @@ local function logics(entity_list, target_selector_data, best_target)
         end
     end
 
-    if not constains_relevant and area_data.score < menu_elements_sorc_base.soft_score:get() and not keybind_can_skip  then
+    if not constains_relevant and area_data.score < menu_elements_sorc_base.soft_score:get() and not keybind_can_skip then
         return false;
     end
 
-    -- todo in future avg weight vec3 (?)
     local cast_position = area_data.main_target:get_position();
     local cast_position_distance_sqr = cast_position:squared_dist_to_ignore_z(player_position);
-    if cast_position_distance_sqr < 2.0 and not keybind_can_skip  then
+    if cast_position_distance_sqr < 2.0 and not keybind_can_skip then
         return false;
     end
 
     cast_spell.position(spell_id_tp, cast_position, 0.3);
-    local current_time = get_time_since_inject();
     next_time_allowed_cast = current_time + 0.4;
         
     console.print("Sorcerer Plugin, Casted Tp");
     return true;
-
 end
 
 return 
